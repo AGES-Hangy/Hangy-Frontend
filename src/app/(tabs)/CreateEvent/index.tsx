59408333751097fa83a504dcf75ed61357bf87b9
acpurper <acpurper@gmail.com>
2026-08-31 18:42:59 -0300

    Começo da tela Criar Evento 1 - Nome e descrição do evento

--- src/app/(tabs)/CreateEvent/index.tsx
+++ src/app/(tabs)/CreateEvent/index.tsx
@@ -1,25 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-import { colors } from '@/constants/colors';
+import { Redirect} from 'expo-router';
 
 export default function CreateEvent() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Create Event</Text>
-    </View>
-  );
+  return <Redirect href="/CriarEventoPt1" />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: colors.action.primary,
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: '700',
-    color: colors.action.secondary,
-  },
-});
